6c27f5443e4d011ee4857015ec773d36ead3dddb
dkluver <dkluver@macalester.edu>
2017-01-12 15:17:24 -0600

    Updated iml file with more dependencies and added a java version check to HelloWorld

--- src/comp124/HelloWorld.java
+++ src/comp124/HelloWorld.java
@@ -11,6 +11,14 @@ public class HelloWorld {
      * This main method starts running the program
      */
     public static void main(String[] args){
+        // print a traditional greeting
         System.out.println("Hello World");
+
+        // print out some diagnostic information
+        String javaVersion = System.getProperty("java.version"); // the version of java being used
+        System.out.println("You are using java version: '"+javaVersion+"'");
+        System.out.println("The string shown should start with '1.8'");
+        System.out.println("If it does not you are likely not using the correct version of java for our class.");
+
     }
 }
