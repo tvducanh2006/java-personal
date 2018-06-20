e9003913c3018d5bb81d24ba3876b0e4676187d0
tvducanh2006 <thaivuducanh@gmail.com>
2018-06-20 11:47:34 -0500

    FINISH HW0

--- src/basicJava/OddEven.java
+++ src/basicJava/OddEven.java
@@ -1,4 +1,17 @@
 package basicJava;
 
+import java.util.Scanner;
+
 public class OddEven {
+    public static void main(String[] args) {
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Please input a number: ");
+        int inputNumber = scan1.nextInt();
+        if (inputNumber % 2 == 1){
+            System.out.println("This is an odd number.");
+        }
+        else {
+            System.out.println("This is an even number.");
+        }
+    }
 }
