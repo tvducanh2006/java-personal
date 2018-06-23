ebc9ca3b4b5167858390098eed79b69c52efe08e
tvducanh2006 <thaivuducanh@gmail.com>
2018-06-23 11:25:12 -0500

    FINISH ACTIVITY3

--- src/basicJava/DoNDrills.java
+++ src/basicJava/DoNDrills.java
@@ -1,4 +1,30 @@
 package basicJava;
 
 public class DoNDrills {
+    public static void main(String[] args) {
+        for (int i=0 ; i<10 ; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i=0 ; i<11 ; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i=10 ; i>=0 ; i--){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i=1 ; i<33 ; i*=2){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i=1 ; i<26 ; i+=2){
+            System.out.print(Math.sqrt(i) + " ");
+        }
+        System.out.println();
+    }
 }
