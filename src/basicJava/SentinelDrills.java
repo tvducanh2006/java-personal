e824a9ca86c7fef7f29248cff881eab62019bb93
tvducanh2006 <thaivuducanh@gmail.com>
2018-06-25 10:20:00 -0500

    FINISH ACTIVITY3

--- src/basicJava/SentinelDrills.java
+++ src/basicJava/SentinelDrills.java
@@ -1,6 +1,17 @@
 package basicJava;
 
+import java.util.Scanner;
+
 public class SentinelDrills {
     public static void main(String[] args) {
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Please input a number: ");
+        int inputNumber = scan1.nextInt();
+        int times = 0;
+        while (inputNumber != 1){
+            times++;
+            inputNumber /= 2;
+        }
+        System.out.println("The times we needed to divide is " + times);
     }
 }
