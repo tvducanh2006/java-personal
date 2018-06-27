db12849069bd4a3f632b32220fef5a258f916ca2
tvducanh2006 <thaivuducanh@gmail.com>
2018-06-27 12:23:29 -0500

    FINISH ACTIVITY4

--- src/basicJava/DoNDrills.java
+++ src/basicJava/DoNDrills.java
@@ -1,5 +1,7 @@
 package basicJava;
 
+import java.util.Scanner;
+
 public class DoNDrills {
     public static void main(String[] args) {
         for (int i=0 ; i<10 ; i++){
@@ -26,5 +28,63 @@ public class DoNDrills {
             System.out.print(Math.sqrt(i) + "; ");
         }
         System.out.println();
+
+        for (int i=10 ; i>0 ; i--){
+            System.out.print(i + "; ");
+        }
+        System.out.println();
+
+        for (int i=32 ; i>0 ; i/=2){
+            System.out.print(i + "; ");
+        }
+        System.out.println();
+
+        for (int i=2 ; i<11 ; i++){
+            System.out.print("1/ " + i + "; ");
+        }
+        System.out.println();
+
+        // First way
+//        Scanner scan1 = new Scanner(System.in);
+//        System.out.println("Please input a number: ");
+//        int numRows = scan1.nextInt();
+//        for(int row = 1 ; row < numRows+1 ; row++){
+//            for(int space = numRows-row; space>0 ; space--){
+//                System.out.print(" ");
+//            }
+//            for(int star = 1 ; star < row*2 ; star++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        // Second way
+        Scanner scan1= new Scanner(System.in);
+        System.out.println("Please input a number: ");
+        int numRows = scan1.nextInt();
+        int maxStars = 2*numRows;
+        for(int starsInRow=1 ; starsInRow < maxStars ; starsInRow+=2) {
+            for (int numSpace = numRows - 1; numSpace > 0; numSpace--) {
+                System.out.print(" ");
+            }
+            numRows--;
+            for (int numStars = 0; numStars < starsInRow; numStars++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=0 ; i<5 ; i++){
+            System.out.print("  X");
+        }
+        System.out.println();
+        System.out.print("  ");
+        for(int i=0 ; i<4 ; i++){
+            System.out.print("  X");
+        }
+        System.out.println();
+        for(int i=0 ; i<5 ; i++){
+            System.out.print("  X");
+        }
     }
 }
--- src/basicJava/SentinelDrills.java
+++ src/basicJava/SentinelDrills.java
@@ -6,12 +6,30 @@ public class SentinelDrills {
     public static void main(String[] args) {
         Scanner scan1 = new Scanner(System.in);
         System.out.println("Please input a number: ");
-        int inputNumber = scan1.nextInt();
+        int inputNumber1 = scan1.nextInt();
         int times = 0;
-        while (inputNumber != 1){
+        while (inputNumber1 != 1){
             times++;
-            inputNumber /= 2;
+            inputNumber1 /= 2;
         }
         System.out.println("The times we needed to divide is " + times);
+
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("Please input a number: ");
+        int inputNumber2 = scan2.nextInt();
+        while (inputNumber2 % 4 != 0){
+            System.out.println(Math.sqrt(inputNumber2));
+        }
+
+        Scanner scan4 = new Scanner(System.in);
+        System.out.println("What do you want to say?(stop to stop): ");
+        String inputString1 = scan4.nextLine();
+        while (inputString1 != "stop"){
+            System.out.print(inputString1 + "; ");
+        }
+        System.out.println();
+
+
+
     }
 }
